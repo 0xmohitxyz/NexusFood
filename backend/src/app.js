7e30530826faf4088ba2927fdc37f9db63089a89
VISHALX33 <vishalprajapati2303@gmail.com>
2026-03-06 00:22:39 +0530

    Vishal branch

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,20 +4,27 @@ const authRoutes = require('./routes/auth.routes');
 const foodRoutes = require('./routes/food.routes');
 const foodPartnerRoutes = require('./routes/food-partner.routes');
 const cors = require('cors');
+
 const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors ({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
 app.get("/", (req, res) => {
-    res.send("Hello world");
-})
+  res.send("Hello world");
+});
 
 app.use('/api/auth', authRoutes);
-
 app.use('/api/food', foodRoutes);
 app.use('/api/food-partner', foodPartnerRoutes);
+
 module.exports = app;
